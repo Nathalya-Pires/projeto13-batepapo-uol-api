896902ef4fa8c149c6ce5e975a4f8fa92029e0f8
Nathalya Ohana Pires Ferreira <nathyohana@gmail.com>
2023-01-15 18:11:59 -0400

    Req: armz dados, post e get participants

--- src/server.js
+++ src/server.js
@@ -2,16 +2,16 @@ import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
-
+import dayjs from "dayjs";
 dotenv.config();
 
-const MongoClient = new MongoClient(process.env.DATABASE_URL);
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
 
 let db;
 
 try {
-  await MongoClient.connect();
-  db = MongoClient.db();
+  await mongoClient.connect();
+  db = mongoClient.db();
 } catch (error) {
   console.log(error);
 }
@@ -20,13 +20,42 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
-// server.post("/participants", async (req, res) => {
-//     const {name} = req.body;
+server.post("/participants", async (req, res) => {
+  const { name } = req.body;
+  const hour = dayjs().format("HH:mm:ss");
 
-// })
+  try {
+    const existName = await db.collection("participants").findOne({ name });
 
+    if (existName)
+      return res.status(409).send("Já existe um participante com este nome");
 
+    await db
+      .collection("participants")
+      .insertOne({ name, lastStatus: Date.now() });
+    res.sendStatus(201);
+    await db.collection("messages").insertOne({
+      from: name,
+      to: "Todos",
+      text: "entra na sala...",
+      type: "status",
+      time: hour,
+    });
+    return res.sendStatus(201);
+  } catch (error) {
+    return res.sendStatus(500);
+  }
+});
+
+server.get("/participants", async (req, res) => {
+  try {
+    const users = await db.collection("participants").find().toArray();
+    res.send(users);
+  } catch (error) {
+    return res.sendStatus(500);
+  }
+});
 
 const PORT = 5000;
 
-server.listen(PORT, () => console.log("Conectado na porta" + { PORT }));
+server.listen(PORT, () => console.log(`Conectado na porta ${PORT}`));
