c9cafb68f6de1eebbe6962687dff4b1a6c02002d
Nathalya Ohana Pires Ferreira <nathyohana@gmail.com>
2023-01-15 03:29:42 -0400

    atualização

--- src/server.js
+++ src/server.js
@@ -1,16 +1,32 @@
-import express from "express"
-import cors from "cors"
-import {MongoClient} from "mongodb"
-import dotenv from "dotenv"
+import express from "express";
+import cors from "cors";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const MongoClient = new MongoClient(process.env.DATABASE_URL)
+const MongoClient = new MongoClient(process.env.DATABASE_URL);
 
-const server = express()
-server.use(cors())
-server.use(express.json())
+let db;
 
-const PORT = 5000
+try {
+  await MongoClient.connect();
+  db = MongoClient.db();
+} catch (error) {
+  console.log(error);
+}
 
-server.listen(PORT, () => console.log('Conectado na porta' + {PORT}))
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+// server.post("/participants", async (req, res) => {
+//     const {name} = req.body;
+
+// })
+
+
+
+const PORT = 5000;
+
+server.listen(PORT, () => console.log("Conectado na porta" + { PORT }));
